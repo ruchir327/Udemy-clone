c6dfe9ec7f796d8c5752c6a76ff8da09cac98ec9
ruchirshrikhande <ruchir.shrikhande555@gmail.com>
2023-08-01 11:58:53 -0400

    push client and server

---
+++ client/pages/login.js
@@ -0,0 +1,106 @@
+import { useState, useContext, useEffect } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { SyncOutlined } from "@ant-design/icons";
+import Link from "next/link";
+import { Context } from "../context";
+import { useRouter } from "next/router";
+import {Button} from 'antd';
+const Login = () => {
+  const [email, setEmail] = useState("ruchir.shrikhande555@gmail.com");
+  const [password, setPassword] = useState("MongoDb123$");
+  const [loading, setLoading] = useState(false);
+
+  // state
+  const {
+    state: { user },
+    dispatch,
+  } = useContext(Context);
+  // const { user } = state;
+
+  // router
+  const router = useRouter();
+
+  useEffect(() => {
+    if (user !== null) router.push("/");
+  }, [user]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // console.table({ name, email, password });
+    try {
+      setLoading(true);
+      const { data } = await axios.post(`/api/login`, {
+        email,
+        password,
+      });
+      // console.log("LOGIN RESPONSE", data);
+      dispatch({
+        type: "LOGIN",
+        payload: data,
+      });
+      // save in local storage
+      window.localStorage.setItem("user", JSON.stringify(data));
+      toast("Logged in successfully!")
+      // redirect
+      router.push("/");
+      // setLoading(false);
+    } catch (err) {
+      toast(err.response.data);
+      setLoading(false);
+    }
+  };
+
+  return (
+    <>
+      <h1 className="jumbotron text-center bg-primary square">Login</h1>
+
+      <div className="container col-md-6 offset-md-3 pb-5">
+        <form >
+          <input
+            type="email"
+            className="form-control mb-4 p-4"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Enter email"
+            required
+          />
+
+          <input
+            type="password"
+            className="form-control mb-4 p-4"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Enter password"
+            required
+          />
+
+          <Button
+            type="primary"
+            className="btn btn-block btn-primary"
+            size="large"
+            disabled={!email || !password || loading}
+            onClick={handleSubmit}
+          >
+            {loading ? <SyncOutlined spin /> : "Submit"}
+          </Button>
+        </form>
+
+        <p className="text-center pt-3">
+          Not yet registered?{" "}
+          <Link href="/register">
+            <a>Register</a>
+          </Link>
+        </p>
+
+        <p className="text-center">
+          <Link href="/forgot-password">
+            <a className="text-danger">Forgot password</a>
+          </Link>
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default Login;
---
+++ client/pages/register.js
@@ -0,0 +1,100 @@
+import { useState, useEffect, useContext } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { SyncOutlined } from "@ant-design/icons";
+import Link from "next/link";
+import { Context } from "../context";
+import { useRouter } from "next/router";
+import user from "../../server/models/user";
+import { Button } from "antd";
+const Register = () => {
+  const [name, setName] = useState("Ruchir");
+  const [email, setEmail] = useState("ruchir.shrikhande555@gmail.com");
+  const [password, setPassword] = useState("rrrrrr");
+  const [loading, setLoading] = useState(false);
+
+  const {
+    state: { user },
+  } = useContext(Context);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    if (user !== null) router.push("/");
+  }, [user]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // console.table({ name, email, password });
+    try {
+      setLoading(true);
+      const { data } = await axios.post(`/api/register`, {
+        name,
+        email,
+        password,
+      });
+      // console.log("REGISTER RESPONSE", data);
+      toast("Registration successful. Please login.");
+      setLoading(false);
+    } catch (err) {
+      toast(err.response.data);
+      setLoading(false);
+    }
+  };
+
+  return (
+    <>
+      <h1 className="jumbotron text-center square">Register</h1>
+
+      <div className="container col-md-6 offset-md-3 ">
+        <form>
+          <input
+            type="text"
+            className="form-control mb-4 p-4"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Enter name"
+            required
+          />
+
+          <input
+            type="email"
+            className="form-control mb-4 p-4"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Enter email"
+            required
+          />
+
+          <input
+            type="password"
+            className="form-control mb-4 p-4"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Enter password"
+            required
+          />
+
+          <Button
+            type="submit"
+            className="btn btn-block btn-primary"
+            
+            disabled={!name || !email || !password || loading}
+          >
+            {loading ? <SyncOutlined spin /> : "Submit"}
+            onClick={handleSubmit}
+          </Button>
+        </form>
+
+        <p className="text-center p-3">
+          Already registered?{" "}
+          <Link href="/login">
+            <a>Login</a>
+          </Link>
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default Register;
